bd46f1d9da3b329b22423b5e71f10a202fc8d5d4
GastonNP <gastonrock8@hotmail.com>
2017-05-16 19:35:12 +0200

    En caso de que sea Árbol Binario.

---
+++ heap_abb.c
@@ -0,0 +1,94 @@
+#include <stdbool.h>  /* bool */
+#include <stddef.h>	  /* size_t */
+
+#define TAMANIO_INICIAL 8
+
+/* ******************************************************************
+ *                DEFINICION DE LOS TIPOS DE DATOS
+ * *****************************************************************/
+
+struct heap{
+    struct nodo* raiz;
+    size_t cantidad;
+    cmp_func_t comparador;
+};
+
+struct nodo{
+    struct nodo* izq;
+    struct nodo* der;
+    void* dato;
+};
+typedef struct nodo nodo_t;
+
+/* ******************************************************************
+ *                			FUNCIONES APARTE
+ * *****************************************************************/
+
+/* Función de heapsort genérica. Esta función ordena mediante heap_sort
+ * un arreglo de punteros opacos, para lo cual requiere que se
+ * le pase una función de comparación. Modifica el arreglo "in-place".
+ * Nótese que esta función NO es formalmente parte del TAD Heap.
+ */
+ //TODO: Implement me!
+void heap_sort(void *elementos[], size_t cant, cmp_func_t cmp) {
+	
+}
+
+/* ******************************************************************
+ *                    	PRIMITIVAS DEL HEAP
+ * *****************************************************************/
+
+heap_t *heap_crear(cmp_func_t cmp) {
+	heap_t* heap = malloc(sizeof(heap_t));
+    if (!heap)
+        return NULL;
+    heap->comparador = cmp;
+    heap->raiz = NULL;
+    heap->cantidad = 0;
+    return heap;
+}
+
+heap_t *heap_crear_arr(void *arreglo[], size_t n, cmp_func_t cmp) {
+	heap_t* heap = heap_crear();
+	
+	for(int i = 0; i < n, i++) {
+		heap_encolar(heap, arreglo[i]);
+	}
+	
+	return heap;
+}
+
+void heap_destruir(heap_t *heap, void destruir_elemento(void *e)) {
+	nodo_t* buffer;
+	
+	while(heap->raiz) {
+		buffer = heap_desencolar(heap);
+		if(destruir_elemento) {
+			destruir_elemento(buffer->dato);
+			free(buffer);
+		}
+	}
+}
+
+
+size_t heap_cantidad(const heap_t *heap) {
+	return heap->cantidad;
+}
+
+bool heap_esta_vacio(const heap_t *heap) {
+	return (bool) heap->raiz;
+}
+
+//TODO: Implement me!
+bool heap_encolar(heap_t *heap, void *elem) {
+	return true;
+}
+
+void *heap_ver_max(const heap_t *heap) {
+	return heap->raiz;
+}
+
+//TODO: Implement me!
+void *heap_desencolar(heap_t *heap) {
+	return NULL;
+}
