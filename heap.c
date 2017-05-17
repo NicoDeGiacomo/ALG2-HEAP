7567fba3fe387b1ebfd9250e04ebbef514ac5c58
NicoDeGiacomo <nicolas.de.giacomo@gmail.com>
2017-05-16 23:20:44 -0300

    Some Fixes

--- heap.c
+++ heap.c
@@ -1,7 +1,10 @@
 #include <stdbool.h>  /* bool */
 #include <stddef.h>	  /* size_t */
+#include <stdlib.h>
+#include "heap.h"
 
-#define TAMANIO_INICIAL 8
+#define TAMANIO_INICIAL 500
+
 
 /* ******************************************************************
  *                DEFINICION DE LOS TIPOS DE DATOS
@@ -13,31 +16,25 @@
     cmp_func_t comparador;
 };
 
+
 /* ******************************************************************
  *                			FUNCIONES APARTE
  * *****************************************************************/
 
-/* Función de heapsort genérica. Esta función ordena mediante heap_sort
- * un arreglo de punteros opacos, para lo cual requiere que se
- * le pase una función de comparación. Modifica el arreglo "in-place".
- * Nótese que esta función NO es formalmente parte del TAD Heap.
- */
 //TODO: Implement me!
 void heap_sort(void *elementos[], size_t cant, cmp_func_t cmp) {
 	
 }
 
 bool heap_redimensionar(heap_t* heap, size_t tam_nuevo) {
+	//TODO: CASO DE FALLO
     void** tablaBuffer = realloc(heap->tabla, tam_nuevo * sizeof(void*));
 
-    if (tam_nuevo && !datos_nuevo) {
-        return false;
-    }
-	
     heap->tabla = tablaBuffer;
     heap->cantidad = tam_nuevo;
     return true;
 }
+
 
 /* ******************************************************************
  *                    	PRIMITIVAS DEL HEAP
@@ -54,9 +51,9 @@ heap_t *heap_crear(cmp_func_t cmp) {
 }
 
 heap_t *heap_crear_arr(void *arreglo[], size_t n, cmp_func_t cmp) {
-	heap_t* heap = heap_crear();
+	heap_t* heap = heap_crear(cmp);
 	
-	for(int i = 0; i < n, i++) {
+	for(int i = 0; i < n; i++) {
 		heap_encolar(heap, arreglo[i]);
 	}
 	
@@ -64,23 +61,22 @@ heap_t *heap_crear_arr(void *arreglo[], size_t n, cmp_func_t cmp) {
 }
 
 void heap_destruir(heap_t *heap, void destruir_elemento(void *e)) {
-	void* buffer;
-	
-	while(heap->tabla[0]) {
-		buffer = heap_desencolar(heap);
+
+	while(!heap_esta_vacio(heap)) {
+        void* buffer = heap_desencolar(heap);
 		if(destruir_elemento)
 			destruir_elemento(buffer);
 	}
+
 	free(heap);
 }
-
 
 size_t heap_cantidad(const heap_t *heap) {
 	return heap->cantidad;
 }
 
 bool heap_esta_vacio(const heap_t *heap) {
-	return (bool) heap_cantidad(heap);
+	return !heap_cantidad(heap);
 }
 
 //TODO: Implement me!
@@ -89,6 +85,8 @@ bool heap_encolar(heap_t *heap, void *elem) {
 }
 
 void *heap_ver_max(const heap_t *heap) {
+    if (heap_esta_vacio(heap))
+        return NULL;
 	return heap->tabla[0];
 }
 
