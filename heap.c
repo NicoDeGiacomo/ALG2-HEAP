ac6228d3d1f5d1d552e16b53dbcaaee8099a85d6
GastonNP <gastonrock8@hotmail.com>
2017-05-18 16:09:02 -0300

    Update heap_sort, redim

--- heap.c
+++ heap.c
@@ -7,7 +7,8 @@
 #define PADRE(i) (i-1)/2
 #define HIJO_IZQ(i) (2*i)+1
 #define HIJO_DER(i) (2*i)+2
-
+#define COEF_REDIM 0.7
+#define FACT_REDIM 2
 
 /* ******************************************************************
  *                DEFINICION DE LOS TIPOS DE DATOS
@@ -16,6 +17,7 @@
  struct heap{
     void** tabla;
     size_t cantidad;
+	size_t tamanio;
     cmp_func_t comparador;
 };
 
@@ -27,9 +29,19 @@ void up_heap(heap_t* heap);
 void down_heap(heap_t* heap);
 void swap(heap_t* heap, size_t indice1, size_t indice2);
 
-//TODO: Implement me!
+//TODO: Revisar.
 void heap_sort(void *elementos[], size_t cant, cmp_func_t cmp) {
+	heap_t *buffer = heap_crear_arr(elementos, cant, cmp);
 	
+	if(!buffer || !buffer->tabla)
+		return;
+	
+	for(int i = 0; i < cant; i++) {
+		elementos[i] = buffer->tabla[i];
+	}
+	
+	free(buffer->tabla);
+	free(buffer);
 }
 
 bool heap_redimensionar(heap_t* heap, size_t tam_nuevo) {
@@ -51,15 +63,19 @@ heap_t *heap_crear(cmp_func_t cmp) {
 	heap_t* heap = malloc(sizeof(heap_t));
     if (!heap)
         return NULL;
+	
     heap->comparador = cmp;
     heap->tabla = malloc(sizeof(void*) * TAMANIO_INICIAL);
     heap->cantidad = 0;
+	heap->tamanio = TAMANIO_INICIAL;
     return heap;
 }
 
 heap_t *heap_crear_arr(void *arreglo[], size_t n, cmp_func_t cmp) {
 	heap_t* heap = heap_crear(cmp);
-
+	if(!heap)
+		return NULL;
+	
 	for(int i = 0; i < n; i++) {
 		heap_encolar(heap, arreglo[i]);
 	}
@@ -87,7 +103,12 @@ bool heap_esta_vacio(const heap_t *heap) {
 }
 
 bool heap_encolar(heap_t *heap, void *elem) {
-    //TODO: Controlar la redimension
+	
+	if((double) heap->cantidad / (double) heap->tamanio >= COEF_REDIM) {
+		if(!heap_redimensionar(heap, heap->tamanio * FACT_REDIM))
+			return false;
+	}
+	
     heap->tabla[heap->cantidad] = elem;
     heap->cantidad++;
     up_heap(heap);
@@ -105,13 +126,15 @@ void *heap_desencolar(heap_t *heap) {
     if(heap_esta_vacio(heap))
         return NULL;
 
-    //TODO: Controlar la redimension
     void* buffer = heap->tabla[0];
     heap->tabla[0] = heap->tabla[heap->cantidad-1];
     heap->tabla[heap->cantidad-1] = NULL;
     heap->cantidad--;
     down_heap(heap);
 
+	if((double) heap->cantidad / (double) heap->tamanio <= heap->tamanio / 4) 
+		heap_redimensionar(heap, heap->tamanio / FACT_REDIM);
+	
     return buffer;
 }
 
