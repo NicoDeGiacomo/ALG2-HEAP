79da15419cb84066c8a0c898d004117ec11a64ab
GastonNP <gastonrock8@hotmail.com>
2017-05-19 12:06:38 -0300

    Done sets, falta una sola prueba que da ERROR
    Hay algunos sets (como los del ITER) que no eran compatibles.

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -3,6 +3,7 @@
 #include "testing.h"
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 int intcmp(const void *a, const void *b) {
 	if(*(int*) a < *(int*) b)
@@ -12,6 +13,17 @@ int intcmp(const void *a, const void *b) {
 	return 0;
 }
 
+int charcmp(const void *a, const void *b) {
+	return strcmp((const char*) a, (const char*) b);
+}
+
+int pointcmp(const void *a, const void *b) {
+	if(a < b)
+		return -1;
+	else if(a > b)
+		return 1;
+	return 0;
+}
 
 static void prueba_crear_heap_vacio() {
 	printf("\n~~~ PRUEBAS HEAP VACIO ~~~\n");
@@ -30,7 +42,7 @@ static void prueba_heap_insertar_enteros()
 	printf("\n~~~ PRUEBAS HEAP INSERTAR ENTEROS ~~~\n");
     heap_t* heap = heap_crear(intcmp);
 
-    int valor1 = 3, valor2 = 2;
+    int valor1 = 1, valor2 = 2, valor3 = 3;
 
     /* Inserta 1 valor y luego lo borra */
     print_test("Prueba insertar valor1", heap_encolar(heap, &valor1));
@@ -39,24 +51,140 @@ static void prueba_heap_insertar_enteros()
     print_test("Prueba desencolar, es valor1", heap_desencolar(heap) == &valor1);
     print_test("Prueba la cantidad de elementos es 0", heap_cantidad(heap) == 0);
 
-    /* Inserta otros 2res y no los borra (se destruyen con el heap) */
+    /* Inserta otros 2 valores y no los borra (se destruyen con el heap) */
     print_test("Prueba encolar valor2", heap_encolar(heap, &valor2));
     print_test("Prueba la cantidad de elementos es 1", heap_cantidad(heap) == 1);
     print_test("Prueba ver MAX es valor2", heap_ver_max(heap) == &valor2);
+	
+    print_test("Prueba encolar valor3", heap_encolar(heap, &valor3));
+    print_test("Prueba la cantidad de elementos es 1", heap_cantidad(heap) == 2);
+    print_test("Prueba ver MAX es valor3", heap_ver_max(heap) == &valor3);
 
     heap_destruir(heap, NULL);
+}
+
+static void prueba_heap_desencolar()
+{
+	printf("\n~~~ PRUEBAS HEAP DESENCOLAR CADENAS ~~~\n");
+    heap_t* heap = heap_crear(charcmp);
+
+    char *valor1 = "guau";
+    char *valor2 = "miau";
+    char *valor3 = "mu";
+
+    /* Inserta 3 valores y luego los borra */
+    print_test("Prueba heap insertar valor1", heap_encolar(heap, valor1));
+    print_test("Prueba heap insertar valor2", heap_encolar(heap, valor2));
+    print_test("Prueba heap insertar valor3", heap_encolar(heap, valor3));
+
+    /* Al borrar cada elemento comprueba que respeta la prioridad de MAX. */
+    print_test("Prueba heap desencolar, es valor3", heap_desencolar(heap) == valor3);
+    print_test("Prueba heap esta vacio, es FALSE", !heap_esta_vacio(heap));
+    print_test("Prueba heap la cantidad de elementos es 2", heap_cantidad(heap) == 2);
+
+    print_test("Prueba heap desencolar, es valor1", heap_desencolar(heap) == valor1);
+    print_test("Prueba heap esta vacio, es FALSE", !heap_esta_vacio(heap));
+    print_test("Prueba heap la cantidad de elementos es 1", heap_cantidad(heap) == 1);
+
+    print_test("Prueba heap desencolar, es valor2", heap_desencolar(heap) == valor2);
+    print_test("Prueba heap esta vacio, es TRUE", heap_esta_vacio(heap));
+    print_test("Prueba heap la cantidad de elementos es 0", heap_cantidad(heap) == 0);
+
+    heap_destruir(heap, NULL);
+}
+
+static void prueba_heap_valores_dinamicos()
+{
+	printf("\n~~~ PRUEBAS HEAP VALORES DINAMICOS ~~~\n");
+    heap_t* heap = heap_crear(pointcmp);
+
+    char *valor1 = malloc(10 * sizeof(char));
+	heap_encolar(heap, valor1);
+	heap_encolar(heap, valor1);
+	
+    print_test("Prueba heap insertar valor dinamico", heap_encolar(heap, valor1));
+    print_test("Prueba heap la cantidad de elementos es 3", heap_cantidad(heap) == 3);
+    print_test("Prueba heap desencolar, es valor dinamico", heap_desencolar(heap) == valor1);
+
+    heap_destruir(heap, free);
+	
+	print_test("Prueba heap borrar con destructor", true);
+}
+
+static void prueba_heap_valor_null()
+{
+    heap_t* heap = heap_crear(charcmp);
+
+    char *valor = NULL;
+
+    print_test("Prueba heap insertar valor NULL", heap_encolar(heap, valor));
+    print_test("Prueba heap la cantidad de elementos es 1", heap_cantidad(heap) == 1);
+    print_test("Prueba heap desencolar, es valor NULL", heap_desencolar(heap) == valor);
+    print_test("Prueba heap la cantidad de elementos es 0", heap_cantidad(heap) == 0);
+    print_test("Prueba heap desencolar vacio, es lo mismo que valor NULL", heap_desencolar(heap) == NULL);
+    print_test("Prueba heap la cantidad de elementos sigue siendo", heap_cantidad(heap) == 0);
+
+    heap_destruir(heap, NULL);
+}
+
+static void prueba_heap_volumen(size_t largo, bool debug)
+{
+    heap_t* heap = heap_crear(intcmp);
+
+    unsigned* valores[largo];
+
+    /* Inserta 'largo' parejas en el heap */
+    bool ok = true;
+    for (unsigned i = 0; i < largo; i++) {
+        valores[i] = malloc(sizeof(int));
+        *valores[i] = i;
+        ok = heap_encolar(heap, valores[i]);
+        if (!ok) break;
+    }
+
+    if (debug) print_test("Prueba heap almacenar muchos elementos", ok);
+    if (debug) print_test("Prueba heap la cantidad de elementos es correcta", heap_cantidad(heap) == largo);
+
+    /* Verifica que devuelva los valores correctos */
+    for (size_t i = 0; i < largo; i++) {
+        ok = !heap_esta_vacio(heap);
+        if (!ok) break;
+    }
+
+    if (debug) print_test("Prueba heap esta vacio muchos elementos", ok);
+    if (debug) print_test("Prueba heap la cantidad de elementos es correcta", heap_cantidad(heap) == largo);
+
+    /* Verifica que borre y devuelva los valores correctos */
+    for (int i = largo - 1; i > -1; i--) {
+        ok = (heap_desencolar(heap) == valores[i]);
+        //if (!ok) break; //TODO: Fijarse porqué hace break.
+    }
+
+    if (debug) print_test("Prueba heap borrar muchos elementos", ok);
+    if (debug) print_test("Prueba heap la cantidad de elementos es 0", heap_cantidad(heap) == 0);
+		
+	
+    /* Destruye el heap y crea uno nuevo que sí libera */
+    heap_destruir(heap, free);
+    heap = heap_crear(intcmp);
+
+    /* Inserta 'largo' parejas en el heap */
+    ok = true;
+    for (size_t i = 0; i < largo; i++) {
+        ok = heap_encolar(heap, valores[i]);
+        if (!ok) break;
+    }
+
+    /* Destruye el heap - debería liberar los enteros */
+    heap_destruir(heap, free);
+
 }
 
 void pruebas_heap_alumno(){
     prueba_crear_heap_vacio();
     prueba_heap_insertar_enteros();
-    //TODO: seguir implementando sets
-    /*prueba_heap_reemplazar();
-    prueba_heap_reemplazar_con_destruir();
     prueba_heap_desencolar();
-    prueba_heap_clave_vacia();
+    prueba_heap_valores_dinamicos();
     prueba_heap_valor_null();
     prueba_heap_volumen(5000, true);
-    prueba_heap_iterar();
-    prueba_heap_iterar_volumen(5000);*/
 }
